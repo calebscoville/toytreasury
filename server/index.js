9941ddd768dbe8349dc547b671dcb8693c631439
Caleb Scoville <calebscoville2010@gmail.com>
2019-06-14 14:57:22 -0600

    STRIPE!!!!!!

--- server/index.js
+++ server/index.js
@@ -4,8 +4,16 @@ const express = require('express'),
     massive = require('massive'),
     auth_ctrl = require('./controllers/auth_controller')
     main_ctrl = require('./controllers/main_controller')
+    stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+    bodyParser = require('body-parser')
+    cors = require('cors')
+
 const app = express()
 const { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env
+
+app.use(bodyParser.json());
+app.use(cors())
+
 
 
 app.use(express.json())
@@ -37,3 +45,39 @@ app.put('/auth/edit/:id', auth_ctrl.edit)
 app.delete('/auth/delete/:id', auth_ctrl.delete)
 // app.put('/auth/update', auth_ctrl.update)
 
+app.post('/api/payment', function(req, res, next){
+    //convert amount to pennies
+    const amountArray = req.body.amount.toString().split('');
+    const pennies = [];
+    for (var i = 0; i < amountArray.length; i++) {
+      if(amountArray[i] === ".") {
+        if (typeof amountArray[i + 1] === "string") {
+          pennies.push(amountArray[i + 1]);
+        } else {
+          pennies.push("0");
+        }
+        if (typeof amountArray[i + 2] === "string") {
+          pennies.push(amountArray[i + 2]);
+        } else {
+          pennies.push("0");
+        }
+          break;
+      } else {
+          pennies.push(amountArray[i])
+      }
+    }
+    const convertedAmt = parseInt(pennies.join(''));
+  
+    const charge = stripe.charges.create({
+    amount: convertedAmt, // amount in cents, again
+    currency: 'usd',
+    source: req.body.token.id,
+    description: 'Test charge from react app'
+  }, function(err, charge) {
+      if (err) return res.sendStatus(500)
+      return res.sendStatus(200);
+    // if (err && err.type === 'StripeCardError') {
+    //   // The card has been declined
+    // }
+  });
+  });
--- src/Components/Donate.js
+++ src/Components/Donate.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
 import styled from 'styled-components';
-// import stripe from './stripeKey';
+// import stripe from './STRIPE_PUB_KEY';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+import '../App.css';
 
-export const Donate = () => {
-    return (
+class Donate extends Component {
+    onToken = (token) => {
+      token.card = void 0;
+      console.log('token', token);
+      axios.post('http://localhost:3456/api/payment', { token, amount: 1000 } ).then(response => {
+        alert('we are in business')
+      });
+    }
+  
+    render() {
+      return (
         <div>
         <h1>Donate</h1>
-        <text>Although we are funded by the state, and I'm a multi-billionaire (just kidding, but maybe some day...) we are always trying to buy the best toys and fix the old ones. Every donation is greatly appreciated, sincerely welcomed and graciously recognized. From $0.25 to $9,000,000, we'll take it. Thanks.</text>
-        <Button>Donate</Button>
+         <text>
+             Although we are funded by the state, and I'm a multi-billionaire (just kidding, but maybe some day...) we are always trying to buy the best toys and fix the old ones. Every donation is greatly appreciated, sincerely welcomed and graciously recognized. From $0.25 to $9,000,000, we'll take it. Thanks.
+        </text>      
+          <StripeCheckout
+            label="Make a child the happiest child ever by donating $10 at least 100 times"
+            token={this.onToken}
+            stripeKey={ "pk_test_7zAPpxGG6xwTkDTO55KcZMA4000vP1oDan" }
+            amount={1000}
+          />
         </div>
-        
-    )
-}
-
+      );
+    }
+  }
+  
+  export default Donate;
 
 const Button = styled.button`export default PatronDash
 
@@ -25,4 +45,25 @@ margin: 0 1em;
 padding: 0.25em 1em;`
 ;
 
-export default Donate
+
+
+
+
+
+
+
+// export const Donate = () => {
+//     return (
+//         <div>
+//         <h1>Donate</h1>
+//         <text>Although we are funded by the state, and I'm a multi-billionaire (just kidding, but maybe some day...) we are always trying to buy the best toys and fix the old ones. Every donation is greatly appreciated, sincerely welcomed and graciously recognized. From $0.25 to $9,000,000, we'll take it. Thanks.</text>
+//         <Button>Donate</Button>
+//         </div>
+        
+//     )
+// }
+
+
+
+
+// export default Donate
